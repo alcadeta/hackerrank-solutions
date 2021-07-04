be0eb00abe541fb8dc80c8f069462d49e9aa7d39
Alican Demirtas <alicandev@icloud.com>
2021-07-04 11:14:01 +0100

    Number Line Jumps solutions are refactored.

--- problem-solving/number-line-jumps/solution.cs
+++ problem-solving/number-line-jumps/solution.cs
@@ -1,44 +1,31 @@
-// PROBLEM:
-//
-// You are choreographing a circus show with various animals. For one act, you are given two
-// kangaroos on a number line ready to jump in the positive direction (i.e, toward positive
-// infinity).
-//
-// ∙ The first kangaroo starts at location and moves at a rate of meters per jump.
-// ∙ The second kangaroo starts at location and moves at a rate of meters per jump.
-//
-// You have to figure out a way to get both kangaroos at the same location at the same time as part
-// of the show. If it is possible, return YES, otherwise return NO.
-//
-// Link:    https://www.hackerrank.com/challenges/kangaroo/problem
-// Result:  10/10 Stars
-
-/// My Solution
-namespace HackerRank
+namespace ProblemSolving
 {
     class NumberLineJumps
     {
+        private const string _YES = "YES";
+        private const string _NO = "NO";
+
         public string Solution(int x1, int v1, int x2, int v2)
         {
-            if (x1 == x2 && v1 == v2) return "YES";
+            if (x1 == x2 && v1 == v2) return _YES;
             if (x1 > x2 && v1 > v2
-                || x1 < x2 && v1 < v2
-                || x1 == x2 && (v1 < v2 || v1 > v2))
-                return "NO";
+                    || x1 < x2 && v1 < v2
+                    || x1 == x2 && (v1 < v2 || v1 > v2))
+                return _NO;
 
             if (x1 > x2) while (x1 > x2)
             {
                 x1 += v1;
                 x2 += v2;
-            } if (x1 == x2) return "YES";
+            } if (x1 == x2) return _YES;
 
             if (x1 < x2) while (x1 < x2)
             {
                 x1 += v1;
                 x2 += v2;
-            } if (x1 == x2) return "YES";
+            } if (x1 == x2) return _YES;
 
-            return "NO";
+            return _NO;
         }
     }
 }
--- problem-solving/number-line-jumps/solution.py
+++ problem-solving/number-line-jumps/solution.py
@@ -1,21 +1,6 @@
-# PROBLEM:
-#
-# You are choreographing a circus show with various animals. For one act, you are given two
-# kangaroos on a number line ready to jump in the positive direction (i.e, toward positive
-# infinity).
-#
-# ∙ The first kangaroo starts at location and moves at a rate of meters per jump.
-# ∙ The second kangaroo starts at location and moves at a rate of meters per jump.
-#
-# You have to figure out a way to get both kangaroos at the same location at the same time as part
-# of the show. If it is possible, return YES, otherwise return NO.
-#
-# Link:    https://www.hackerrank.com/challenges/kangaroo/problem
-# Result:  10/10 Stars
-#
-# My Solution:
 YES = "YES"
-NO  = "NO"
+NO = "NO"
+
 
 def numberlinejumps(x1, v1, x2, v2):
     if x1 == x2 and v1 == v2:
