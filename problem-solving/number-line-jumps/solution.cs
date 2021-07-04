eaefe7611ae6483cc09c12c3ee1fa8163970fb52
Alican Demirtas <alicandev@icloud.com>
2021-07-04 11:06:53 +0100

    HackerRank .NET solution and ProblemSolving class library are added for IDE support. C# script files are replaced with regular C# files.

--- problem-solving/number-line-jumps/solution.cs
+++ problem-solving/number-line-jumps/solution.cs
@@ -14,31 +14,31 @@
 // Result:  10/10 Stars
 
 /// My Solution
-string NumberLineJumps(int x1, int v1, int x2, int v2)
+namespace HackerRank
 {
-    if (x1 == x2 && v1 == v2)
-        return "YES";
-
-    if (x1 > x2 && v1 > v2
-            || x1 < x2 && v1 < v2
-            || x1 == x2 && (v1 < v2 || v1 > v2))
-        return "NO";
-
-    if (x1 > x2) while (x1 > x2)
+    class NumberLineJumps
     {
-        x1 += v1;
-        x2 += v2;
+        public string Solution(int x1, int v1, int x2, int v2)
+        {
+            if (x1 == x2 && v1 == v2) return "YES";
+            if (x1 > x2 && v1 > v2
+                || x1 < x2 && v1 < v2
+                || x1 == x2 && (v1 < v2 || v1 > v2))
+                return "NO";
+
+            if (x1 > x2) while (x1 > x2)
+            {
+                x1 += v1;
+                x2 += v2;
+            } if (x1 == x2) return "YES";
+
+            if (x1 < x2) while (x1 < x2)
+            {
+                x1 += v1;
+                x2 += v2;
+            } if (x1 == x2) return "YES";
+
+            return "NO";
+        }
     }
-
-    if (x1 == x2) return "YES";
-
-    if (x1 < x2) while (x1 < x2)
-    {
-        x1 += v1;
-        x2 += v2;
-    }
-
-    if (x1 == x2) return "YES";
-
-    return "NO";
 }
